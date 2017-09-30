e8ca00089859682ec2e620bf235c644ce9ee09d9
hanabata <contrasation805@gmail.com>
2017-09-30 19:38:11 +0900

    Add note.

--- new_app2/bbs/static/bbs/index.js
+++ new_app2/bbs/static/bbs/index.js
@@ -1,29 +1,10 @@
 Vue.use(VueMaterial)
 
 var App = new Vue({
-  el: '#app'
-});
-
-var App2 = new Vue({
-  el: '#app-2'
-});
-
-var App3 = new Vue({
-  el: '#app-3',
-  methods: {
-    showAlert: function(){
-        alert('Hello')
-    }
-  }
-});
-
-var App4 = new Vue({
-  el: '#app-4',
+  el: '#app',
   data: {
-//    message: 'Hello Vue.js!!'
-    user : {
-      address : 'This sentences would be\nsynchronize with below'
+      user : {
+          address : 'This sentences would be\nsynchronize with below'
+        }
     }
-  }
 });
-
--- new_app2/static/bbs/index.js
+++ new_app2/static/bbs/index.js
@@ -1,36 +1,34 @@
 Vue.use(VueMaterial)
 
 var App = new Vue({
-  el: '#app'
-});
-
-var App2 = new Vue({
-  el: '#app-2'
-});
-
-var App3 = new Vue({
-  el: '#app-3',
-  methods: {
-    showAlert: function(){
-        alert('Hello')
-    }
-  }
-});
-
-var App4 = new Vue({
-  el: '#app-4',
+  el: '#app',
   data: {
-//    message: 'Hello Vue.js!!'
-    user : {
-      address : 'This sentences would be\nsynchronize with below'
+      user : {
+          address : 'This sentences would be\nsynchronize with below'
+        }
     }
-  }
 });
-
 //
-//var App6 = new Vue({
-//  el: '#app-6',
-//  data: {
-//    message: 'Hello Vue!'
+//var App2 = new Vue({
+//  el: '#app-2'
+//});
+//
+//var App3 = new Vue({
+//  el: '#app-3',
+//  methods: {
+//    showAlert: function(){
+//        alert('Hello')
+//    }
 //  }
-//})
+//});
+//
+//var App4 = new Vue({
+//  el: '#app-4',
+//  data: {
+////    message: 'Hello Vue.js!!'
+//    user : {
+//      address : 'This sentences would be\nsynchronize with below'
+//    }
+//  }
+//});
+//
